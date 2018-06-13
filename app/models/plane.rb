a0875914f49fb9ac4afbcc3251caaf4748def726
Mandy T <adamantia.t@gmail.com>
2018-06-13 16:00:24 +0200

    geocoded

--- app/models/plane.rb
+++ app/models/plane.rb
@@ -5,6 +5,8 @@ class Plane < ApplicationRecord
   validates :make, presence: true
   validates :description, presence: true
   validates :location, presence: true
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
   #has_many :renters, through: :rentals,  class_name: "User"
 
 end
