bfed46a705a69ecbb543e8edd9c8dda6150da56a
nathalia coutinho <nathccoutinhos@gmail.com>
2018-06-13 12:42:17 +0200

    made some changes on index, map space and created actions for rental controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,19 +1,39 @@
 class RentalsController < ApplicationController
+  before_action :set_rental, only: [:show, :update]
+  before_action :authenticate_user!
 
-#   def new
-#     @plane = Plane.find(params[:plane_id])
-#     @rental = Rental.new
-#   end
+  def index
+    @rentals = Rental.all
+  end
 
-#   def create
-#     @plane = Plane.find(params[:plane_id])
-#     @rental = @plane.rentals.build(rental_params)
-#     if @rental.save
-#       redirect_to plane_path(@plane)
-#     else
-#       render :new
-#     end
-#   end
+  def show
+    @plane = @rental.plane
+  end
+
+  def new
+    @plane = Plane.find(params[:plane_id])
+    @rental = Rental.new
+  end
+
+  def create
+    @rental = Rental.new(rental_params)
+
+    #### using currently logged in user when creating rental (setting it to pending) to set user to rental (owner of the plane)
+    ####
+
+
+    @plane = Plane.find(params[:plane_id])
+    @rental.plane = @plane
+
+    @user = current_user
+    @rental.user = @user
+
+    if @rental.save
+      redirect_to plane_path(@plane), notice: "You booked the plane succefully"
+    else
+      redirect_to new_plane_rental(@rental)
+    end
+  end
 
 
 #   def destroy
@@ -35,5 +55,14 @@ class RentalsController < ApplicationController
 #   #   params.require(:dose).permit(:description, :ingredient_id)
 #   # end
 
+private
+
+  def set_rental
+    @rental = Rental.find(params[:id])
+  end
+
+  def rental_params
+      params.require(:rental).permit(:start_date, :end_date)
+  end
 
 end
