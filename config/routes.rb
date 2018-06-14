eae768b9cfdd8c6ff23d1cd350274b5a9f83449e
nathalia coutinho <nathccoutinhos@gmail.com>
2018-06-14 14:24:14 +0200

    created dashboard for users to see their bookings and own planes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-
-   resources :planes do
-    resources :rentals
+  resources :planes do
+    resources :rentals, except: :destroy
   end
+  resources :rentals, only: :destroy
 
-  # root to: "planes#index"
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'dashboard', to: 'dashboard#show'
 end
