732fec11729eef68f9d32fb216f4994dbadadbc3
Mandy T <adamantia.t@gmail.com>
2018-06-14 17:26:14 +0200

    fix search

--- app/models/plane.rb
+++ app/models/plane.rb
@@ -1,4 +1,6 @@
 class Plane < ApplicationRecord
+  include PgSearch
+
   belongs_to :user #:owner, class_name: "User"
   has_many :rentals
   validates :rate, presence: true
@@ -8,7 +10,11 @@ class Plane < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
   #has_many :renters, through: :rentals,  class_name: "User"
-  include PgSearch
-  multisearchable against: [ :make, :description, :location ]
   mount_uploader :picture, PictureUploader
+
+  pg_search_scope :full_search,
+    against: [ :make, :description, :location],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
